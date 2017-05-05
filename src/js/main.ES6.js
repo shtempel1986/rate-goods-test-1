a752d3c774653662ce99b553fe79d972150ac278
Pavel Ledovskikh <o_gredi@mail.ru>
2017-05-05 11:06:21 +0300

    закончил верстку

--- src/js/main.ES6.js
+++ src/js/main.ES6.js
@@ -2,10 +2,11 @@
  * Created by Павел on 07.04.2017.
  */
 function checkItemNumber(number, quantity) {
-    if (number < 0) return quantity - 1;
+    if (number < 0) return (quantity - 1);
     if (number >= quantity) return 0;
     return number;
 }
+
 function setClasses ($items){
     for(let item of $items){
         for (let i =0; i< $items.length; i++)
@@ -13,6 +14,7 @@ function setClasses ($items){
         $(item).addClass(`gallery-thumbs__item${$(item).attr("data-gallery-item")}`)
     }
 }
+
 function changeNumber($items, increment) {
     for(let item of $items){
         let number = parseInt($(item).attr("data-gallery-item"));
@@ -20,17 +22,20 @@ function changeNumber($items, increment) {
         console.log($(item), checkItemNumber(number+increment, 10));
     }
 }
+
 $(document).ready(() => {
     let $galleryItems = $(".gallery-thumbs__item");
+
     $(".gallery-thumbs__controls_right").click(()=>{
         let $ninthItem = $(".gallery-thumbs__item9").hide();
-        changeNumber($galleryItems, 1);
+        changeNumber($galleryItems, -1);
         setClasses($galleryItems);
         setTimeout(()=>{$ninthItem.show()}, 100);
     });
+
     $(".gallery-thumbs__controls_left").click(()=>{
         let $zeroItem = $(".gallery-thumbs__item0").hide();
-        changeNumber($galleryItems, -1);
+        changeNumber($galleryItems, 1);
         setClasses($galleryItems);
         setTimeout(()=>{$zeroItem.show()}, 100);
     });
